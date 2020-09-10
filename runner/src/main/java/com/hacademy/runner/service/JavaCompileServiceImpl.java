00e7439dc1c0c2863361aed8b24e86bbc8f308a0
hiphop5782 <zdlsz@hanmail.net>
2020-09-10 16:18:07 +0900

    bug fix
    - add double quote to cmd command

--- runner/src/main/java/com/hacademy/runner/service/JavaCompileServiceImpl.java
+++ runner/src/main/java/com/hacademy/runner/service/JavaCompileServiceImpl.java
@@ -61,7 +61,7 @@ public class JavaCompileServiceImpl implements JavaCompileService{
 
 	@Override
 	public JavaResultVO execute(JavaResultVO resultVO) throws IOException, InterruptedException {
-		String javaHome = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe";
+		String javaHome = "\"" + System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe" + "\"";
 		log.info("javaHome = {}", javaHome);
 		String[] command = new String[] {javaHome, resultVO.getClassName()};
 		
