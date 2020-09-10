62a0e7c921fbdf3ecd06b3521cc0a660c6e34c3d
hiphop5782 <zdlsz@hanmail.net>
2020-09-10 16:14:23 +0900

    log append

--- runner/src/main/java/com/hacademy/runner/service/JavaCompileServiceImpl.java
+++ runner/src/main/java/com/hacademy/runner/service/JavaCompileServiceImpl.java
@@ -62,6 +62,7 @@ public class JavaCompileServiceImpl implements JavaCompileService{
 	@Override
 	public JavaResultVO execute(JavaResultVO resultVO) throws IOException, InterruptedException {
 		String javaHome = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe";
+		log.info("javaHome = {}", javaHome);
 		String[] command = new String[] {javaHome, resultVO.getClassName()};
 		
 		//check time
